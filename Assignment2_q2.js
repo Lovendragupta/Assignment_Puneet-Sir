82a3fb76ce25de63facf4399c1dc36bfbf1991fb
Lovendragupta <98386298+Lovendragupta@users.noreply.github.com>
2022-01-28 16:35:58 +0530

    Create Assignment2_q2.js

---
+++ Assignment2_q2.js
@@ -0,0 +1,25 @@
+//Printing numbers from 1 to 30 and if the number is divisible by 15 then Printing "Hello World" and if the number is divisible by 3 then Printing "Hello"
+//and if the number is divisible by 5 then "World".
+let result = "" 
+for(let i = 1; i <=30 ;i++) { 
+   // if dividing i by 3 and 5  
+   // number is divisible by both 3 and 5 
+   if(i % 3 == 0 && i % 5 == 0) { 
+      result = " Hello World"
+      console.log(result); 
+   // else if number is divisible only by 3 
+   } 
+   else if(i % 3 == 0) {  
+      result =  " Hello "
+      console.log(result); 
+   // else if number is divisible only by 5 
+   } else if(i % 5 == 0) { 
+      result =  " World"
+      console.log(result); 
+   }
+   // else print the number itself 
+   else{
+   result = i;
+   console.log(result); 
+   }
+}
